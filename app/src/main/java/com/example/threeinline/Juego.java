3b76124f56343b2403b0aaf776b1a7065ad65770
RicardoGGx <133252795+RicargoGGx@users.noreply.github.com>
2023-10-18 11:24:24 -0600

    Avance código gatito

--- app/src/main/java/com/example/threeinline/Juego.java
+++ app/src/main/java/com/example/threeinline/Juego.java
@@ -1,10 +1,19 @@
 package com.example.threeinline;
 
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Juego extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego);
+
+    }
     @Override
     public void onClick(View v) {
 
--- app/src/main/java/com/example/threeinline/MainActivity.java
+++ app/src/main/java/com/example/threeinline/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.threeinline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,15 +17,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        iniciarJuegoj = (Button) findViewById(R.id.retry);
+        iniciarJuegoj = (Button) findViewById(R.id.startGame);
         iniciarJuegoj.setOnClickListener(this);
         salirj = (Button) findViewById(R.id.exit);
-
-
+        salirj.setOnClickListener(this);
+        salirj.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View salir){
+                System.exit(0);
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.startGame:
+                Intent segundaVentana = new Intent(MainActivity.this, Juego.class);
+                startActivity(segundaVentana);
+        }
     }
 }
